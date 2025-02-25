bb81e4b3428124a4348847c2cb63f7589c3e516f
jarob <jaron.batens@ugent.be>
2025-02-25 13:43:58 +0100

    verder opzetten, types installeren

--- src/backend/backend_opzet/__tests__/index.test.ts
+++ src/backend/backend_opzet/__tests__/index.test.ts
@@ -1,9 +1,9 @@
-const request = require("supertest");
+import request from "supertest";
 import { describe, it, expect } from "vitest";
 import index from '../index.ts';
 
 describe("Express App", () => {
-    it("should return pong for GET /ping", async () => {
+    it("should return pong from GET /ping", async () => {
         const res = await request(index).get("/ping");
         expect(res.status).toBe(200);
         expect(res.body).toEqual({ message: "pong" });
---
+++ src/backend/backend_opzet/__tests__/leerpaden/leerpaden.test.ts
@@ -0,0 +1,6 @@
+import request from "supertest";
+import { describe, it, expect } from "vitest";
+import index from '../../index.ts';
+
+describe("leerpaden", () => {
+});
