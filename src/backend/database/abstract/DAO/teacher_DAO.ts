4b170c6491d364dc538af6f609accedf699d50ab
mvdrputt <Mirval.Vanderputten@UGent.be>
2025-02-25 19:57:26 +0100

    camelCase instead of snake_case

--- src/backend/database/abstract/DAO/teacher_DAO.ts
+++ src/backend/database/abstract/DAO/teacher_DAO.ts
@@ -1,30 +1,32 @@
 import { Teacher } from "../representation/teacher.ts";
 
-interface StudentDao {
+interface TeacherDao {
     // this method create a new teacher and returns the new teacher id.
-    create_teacher(username: string, password: string, email: string, active_language: string): number;
+    createTeacher(username: string, password: string, email: string, active_language: string): number;
 
     // this method delete a teacher with certain id, it returns the same id of the deleted teacher for confirmation that the deletion was a succes.
-    delete_teacher(id: number): number;
+    deleteTeacher(id: number): number;
+
+    //updateTeacher()
 
     // this method changes the password of teacher with certain id.
-    change_password(id: number, old_password: string): void;
+    changePassword(id: number, old_password: string): void;
 
     // this method changes the email of teacher with certain id.
-    change_email(id: number, old_email: string): void;
+    changeEmail(id: number, old_email: string): void;
 
     // this method changes the username of teacher with certain id.
-    change_username(id: number, old_username: string): void;
+    changeUsername(id: number, old_username: string): void;
 
     // this method changes the active language of teacher with certain id.
-    change_active_language(id: number): void;
+    changeActiveLanguage(id: number): void;
 
     // find teacher by id.
-    find_student_by_id(id: number): Teacher;
+    findTeacherById(id: number): Teacher;
 
     // find teacher by username.
-    find_student_by_username(username: string): Teacher;
+    findTeacherByUsername(username: string): Teacher;
 
     // find teacher by email.
-    find_student_by_email(email: string): Teacher;
+    findTeacherByEmail(email: string): Teacher;
 }
