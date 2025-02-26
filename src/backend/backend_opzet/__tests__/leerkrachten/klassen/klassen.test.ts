0554d6ab02ce6c521c00085176f5b8223fd0d8ed
jarob <jaron.batens@ugent.be>
2025-02-26 22:03:37 +0100

    klassenlijst test

--- src/backend/backend_opzet/__tests__/leerkrachten/klassen/klassen.test.ts
+++ src/backend/backend_opzet/__tests__/leerkrachten/klassen/klassen.test.ts
@@ -3,20 +3,20 @@ import {describe, expect, it} from "vitest";
 import index from '../../../index.ts';
 import {is_klassen_link} from "../../hulpfuncties.ts";
 
-describe("leerlingen/klassen", () => {
+describe("leerkrachten/klassen", () => {
     it("krijg lijst van klassen", async () => {
-        const test_leerling: any = {
+        const test_leerkracht: any = {
             "naam": "test",
             "wachtwoord": "test"
         };
         let res = await request(index)
-            .post("/aanmelden/leerlingen")
-            .send(test_leerling);
+            .post("/aanmelden/leerkrachten")
+            .send(test_leerkracht);
         const token: string = res.body["token"];
-        const leerling_link: String = res.body["id"];
-        const leerling_link_einde: string = leerling_link.substring(leerling_link.indexOf("/"));
+        const leerkracht_link: String = res.body["id"];
+        const leerkracht_link_einde: string = leerkracht_link.substring(leerkracht_link.indexOf("/"));
         res = await request(index)
-            .get(leerling_link_einde + "/klassen")
+            .get(leerkracht_link_einde + "/klassen")
             .set(`Authorization`, `Bearer dit is geen geldig token`);
         expect(res.status).toBe(200);
         expect(Array.isArray(res.body)).toBe(true);
