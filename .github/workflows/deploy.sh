e6e3ca67dd6972b762fb44c0a8206da4c308256c
Thorsten De Neve <thorsten.deneve@ugent.be>
2025-02-27 10:52:00 +0100

    docker containers verwijderen voor nieuwe op te zetten bij deployen

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -8,6 +8,9 @@ cd $APP_DIR
 git checkout main
 git pull --force git@github.com:SELab-2/Dwengo-5.git || { echo "Git pull failed"; exit 1; }
 
+cd $APP_DIR
+docker-compose down -v
+
 echo "Building and deploying with Docker Compose..."
 cd $APP_DIR
 docker-compose -f $DOCKER_COMPOSE_FILE up --build -d || { echo "Docker Compose failed"; exit 1; }
