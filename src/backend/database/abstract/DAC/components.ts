1f445112fee6bb9267b0ca1ee6e6c4cf9ca6a0e8
mvdrputt <Mirval.Vanderputten@UGent.be>
2025-02-27 14:00:06 +0100

    gitignore

--- src/backend/database/abstract/DAC/components.ts
+++ src/backend/database/abstract/DAC/components.ts
@@ -1,15 +1,15 @@
-import {AssignmentDAO} from '../DAO/assignmentDAO';
-import {ClassDAO} from '../DAO/classDAO';
-import {ConversationDAO} from '../DAO/conversationDAO';
-import {GroupDAO} from '../DAO/groupDAO';
-import {LearningObjectDAO} from '../DAO/learningObjectDAO';
-import {LearningObjectMetadataDAO} from '../DAO/learningObjectMetadataDAO';
-import {LearningPathDAO} from '../DAO/learningPathDAO';
-import {LearningPathNodeDAO} from '../DAO/learningPathNodeDAO';
-import {MessageDAO} from '../DAO/messageDAO';
-import {StudentDAO} from '../DAO/studentDAO';
-import {SubmissionDAO} from '../DAO/submissionDAO';
-import {TeacherDAO} from '../DAO/teacher_DAO';
+import { AssignmentDAO } from '../DAO/assignmentDAO';
+import { ClassDAO } from '../DAO/classDAO';
+import { ConversationDAO } from '../DAO/conversationDAO';
+import { GroupDAO } from '../DAO/groupDAO';
+import { LearningObjectDAO } from '../DAO/learningObjectDAO';
+import { LearningObjectMetadataDAO } from '../DAO/learningObjectMetadataDAO';
+import { LearningPathDAO } from '../DAO/learningPathDAO';
+import { LearningPathNodeDAO } from '../DAO/learningPathNodeDAO';
+import { MessageDAO } from '../DAO/messageDAO';
+import { StudentDAO } from '../DAO/studentDAO';
+import { SubmissionDAO } from '../DAO/submissionDAO';
+import { TeacherDAO } from '../DAO/teacher_DAO';
 //import TransitionDAO from '../DAO/transitionDAO';
 
 
