e453790970045b4ff5ce0911363a8c8ff6ccb154
jarob <jaron.batens@ugent.be>
2025-02-28 16:25:21 +0100

    prisma client instaleren

--- src/backend/backend_opzet/controllers/leerlingen/leerlingen_controller.ts
+++ src/backend/backend_opzet/controllers/leerlingen/leerlingen_controller.ts
@@ -1,5 +1,6 @@
 import {Request, Response} from "express";
 import {JsonLeerlingObject} from "../../json_objecten.ts";
+import { Prisma, PrismaClient } from '@prisma/client'
 
 export function maak_leerling(req: Request, res: Response) {
     res.status(501);
