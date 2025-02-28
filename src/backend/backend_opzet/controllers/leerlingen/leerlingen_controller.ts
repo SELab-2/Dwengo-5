a787b82daa69fe70585ccf38e83a72207ad6d82e
jarob <jaron.batens@ugent.be>
2025-02-28 20:03:34 +0100

    leerkrachten_controller.ts

--- src/backend/backend_opzet/controllers/leerlingen/leerlingen_controller.ts
+++ src/backend/backend_opzet/controllers/leerlingen/leerlingen_controller.ts
@@ -8,16 +8,22 @@ export async function leerling(req: Request, res: Response) {
         //todo: auth
         let leerling_id_string: string = req.params.leerling_id;
         let leerling_id: number = Number(leerling_id_string);
-        if (isNaN(leerling_id)) return res.status(400).send({error: "geen geldige leerling_id"})
+        if (isNaN(leerling_id)) {
+            res.status(400).send({error: "geen geldige leerling_id"});
+            return;
+        }
         const leerling = await prisma.student.findUnique({
             where: {
                 id: leerling_id
             }
         });
-        if (!leerling) return res.status(404).send({error: "leerling niet gevonden"});
-        return res.status(200).send({name:leerling.username});
+        if (!leerling) {
+            res.status(404).send({error: "leerling niet gevonden"});
+            return;
+        }
+        res.status(200).send({name: leerling.username});
     } catch (e) {
-        return res.status(500).send({error: "interne fout"})
+        res.status(500).send({error: "interne fout"})
     }
 
 }
@@ -27,20 +33,26 @@ export async function verwijder_leerling(req: Request, res: Response) {
         //todo: auth
         let leerling_id_string: string = req.params.leerling_id;
         let leerling_id: number = Number(leerling_id_string);
-        if (isNaN(leerling_id)) return res.status(400).send({error: "geen geldige leerling_id"})
+        if (isNaN(leerling_id)) {
+            res.status(400).send({error: "geen geldige leerling_id"});
+            return;
+        }
         const leerling = await prisma.student.findUnique({
             where: {
                 id: leerling_id
             }
         });
-        if (!leerling) return res.status(404).send({error: "leerling niet gevonden"});
+        if (!leerling) {
+            res.status(404).send({error: "leerling niet gevonden"});
+            return;
+        }
         await prisma.student.delete({
             where: {
                 id: leerling_id
             }
         });
-        return res.status(200).send();
+        res.status(200).send();
     } catch (e) {
-        return res.status(500).send({error: "interne fout"})
+        res.status(500).send({error: "interne fout"})
     }
 }
--- src/backend/backend_opzet/routes/leerkrachten/leerkrachten_router.ts
+++ src/backend/backend_opzet/routes/leerkrachten/leerkrachten_router.ts
@@ -1,10 +1,11 @@
 import {Router} from "express";
 import klassen_router from "./klassen/klassen_router.ts";
-import {verwijder_leerkracht} from "../../controllers/leerkrachten/leerkrachten_controller.ts";
+import {leerkracht, verwijder_leerkracht} from "../../controllers/leerkrachten/leerkrachten_controller.ts";
 
 const router = Router({mergeParams: true})
 export default router
 
 router.use("/:leerkracht_id/klassen", klassen_router)
 
+router.get("/:leerkracht_id", leerkracht);
 router.delete("/:leerkracht_id", verwijder_leerkracht);
