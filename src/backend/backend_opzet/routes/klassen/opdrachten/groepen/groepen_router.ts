18c04a4ee4c5c812c7faf8e6eb056d7ed80542fe
jarob <jaron.batens@ugent.be>
2025-03-01 12:44:02 +0100

    api aanpassen

--- src/backend/backend_opzet/routes/klassen/opdrachten/groepen/groepen_router.ts
+++ src/backend/backend_opzet/routes/klassen/opdrachten/groepen/groepen_router.ts
@@ -1,10 +1,13 @@
 import {Router} from "express";
 import leerlingen_router from "./leerlingen/leerlingen_router.ts";
+import vragen_router from "./vragen/vragen_router.ts";
+
 
 const router = Router({mergeParams: true})
 export default router
 
 router.use("/:groep_id/leerlingen", leerlingen_router)
+router.use("/:groep_id/vragen", leerlingen_router)
 
 router.get("/",);
 router.post("/",);
