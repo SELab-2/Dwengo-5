5c20d406c0fd120d1ed9a16302fc6cf24bd424ff
Nyah Van Wayenberge <nyah.vanwayenberge@ugent.be>
2025-03-02 10:05:22 +0100

    implementatie: klasLeerlingToevoegen, klasLeerlingVerwijderen

--- src/backend/backend_opzet/controllers/klassen/leerlingen/leerlingen_controller.ts
+++ src/backend/backend_opzet/controllers/klassen/leerlingen/leerlingen_controller.ts
@@ -3,15 +3,15 @@ import {PrismaClient} from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-// GET /klassen/{id}/leerlingen
+// GET /klassen/{klas_id}/leerlingen
 export async function klasLeerlingen(req: Request, res: Response) {
     try {
         //todo: auth
         let klasId: number = Number(req.params.klas_id);
 
-        // controleer the id
+        // controleer het id
         if (isNaN(klasId)) {
-            res.status(400).send({error: "geen geldige klasId"});
+            res.status(400).send({error: "geen geldig klasId"});
             return;
         }
 
@@ -34,17 +34,76 @@ export async function klasLeerlingen(req: Request, res: Response) {
         const resultaten = leerlingen.map((leerling) => {
             return `/leerlingen/${leerling.students_id}`
         });
-
         res.status(200).send({leerlingen: resultaten});
+
     } catch (e) {
         res.status(500).send({error: "interne fout"})
     }
 }
 
+// POST /klassen/{klas_id}/leerlingen
 export async function klasLeerlingToevoegen(req: Request, res: Response) {
-    res.status(501);
+    try {
+        //todo: auth
+        let klasId: number = Number(req.params.klas_id);
+        let leerlingUrl: String = req.body.leerling;
+        let leerlingId: number = Number(leerlingUrl.split("/").pop());
+
+        // controleer de ids
+        if (isNaN(klasId)) {
+            res.status(400).send({error: "geen geldig klasId"});
+            return;
+        }
+
+        if (isNaN(leerlingId)) {
+            res.status(400).send({error: "geen geldig leerlingId"});
+            return;
+        }
+
+        // voeg nieuwe leerling toe aan de klas
+        await prisma.classStudent.create({
+            data: {
+                classes_id: klasId,
+                students_id: leerlingId
+            }
+        });
+        res.status(200);
+
+    } catch (e) {
+        res.status(500).send({error: "interne fout"})
+    }
 }
 
+// DELETE /klassen/{klas_id}/leerlingen/{leerling_id}
 export async function klasLeerlingVerwijderen(req: Request, res: Response) {
-    res.status(501);
+    try {
+        //todo: auth
+        let klasId: number = Number(req.params.klas_id);
+        let leerlingId: number = Number(req.params.leerling_id);
+
+        // controleer de ids
+        if (isNaN(klasId)) {
+            res.status(400).send({error: "geen geldig klasId"});
+            return;
+        }
+
+        if (isNaN(leerlingId)) {
+            res.status(400).send({error: "geen geldig leerlingId"});
+            return;
+        }
+
+        // verwijder een leerling uit de klas
+        await prisma.classStudent.delete({
+            where: {
+                classes_id_students_id: {
+                    classes_id: klasId,
+                    students_id: leerlingId
+                }
+            }
+        });
+        res.status(200);
+
+    } catch (e) {
+        res.status(500).send({error: "interne fout"})
+    }
 }
