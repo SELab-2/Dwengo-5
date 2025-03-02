fdb06f3b05cb803b05fe4be5c978bda852c1f42c
Kamiel Moeyersoon <moeyersoon.kamiel@gmail.com>
2025-03-02 11:37:48 +0100

    fix broken code

--- src/backend/backend_opzet/routes/klassen/leerlingen/conversaties/conversaties_router.ts
+++ src/backend/backend_opzet/routes/klassen/leerlingen/conversaties/conversaties_router.ts
@@ -1,13 +1,13 @@
-import {Router} from "express";
+import { Router } from "express";
 import {
-    beantwoord_vraag,
-    leerling_conversaties,
-    vraag,
-    vraag_aanmaken,
-    vraag_verwijder
+  beantwoord_vraag,
+  leerling_conversaties,
+  vraag,
+  vraag_aanmaken,
+  vraag_verwijderen,
 } from "../../../../controllers/klassen/leerlingen/conversaties/conversaties_controller.ts";
 
-const router = Router({mergeParams: true})
-export default router
+const router = Router({ mergeParams: true });
+export default router;
 
 router.get("/", leerling_conversaties);
