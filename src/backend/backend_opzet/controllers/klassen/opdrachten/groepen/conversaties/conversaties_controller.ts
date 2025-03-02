7ac46d1949f6e2c8417e492a77fba47721e42b7c
Nyah Van Wayenberge <nyah.vanwayenberge@ugent.be>
2025-03-02 21:25:44 +0100

    implementatie groepConversaties

--- src/backend/backend_opzet/controllers/klassen/opdrachten/groepen/conversaties/conversaties_controller.ts
+++ src/backend/backend_opzet/controllers/klassen/opdrachten/groepen/conversaties/conversaties_controller.ts
@@ -1,10 +1,52 @@
 import {Request, Response} from "express";
+import {PrismaClient} from "@prisma/client";
 
-export async function groep_conversaties(req: Request, res: Response) {
-    res.status(501);
+const prisma = new PrismaClient();
+
+// GET /klassen/{klas_id}/opdrachten/{opdracht_id}/groepen/{groep_id}/conversaties
+export async function groepConversaties(req: Request, res: Response) {
+    try {
+        //todo: auth
+        let klasId: number = Number(req.params.klas_id);
+        let opdrachtId: number = Number(req.params.opdracht_id);
+        let groepId: number = Number(req.params.groep_id);
+
+        // controlleer de ids
+        if (isNaN(klasId)) {
+            res.status(400).send({error: "geen geldig klasId"});
+            return;
+        }
+        if (isNaN(opdrachtId)) {
+            res.status(400).send({error: "geen geldig opdrachtId"});
+            return;
+        }
+        if (isNaN(groepId)) {
+            res.status(400).send({error: "geen geldig groepId"});
+            return;
+        }
+
+        // alle conversaties over een opdracht van een groep opvragen
+        const conversaties = await prisma.conversation.findMany({
+            where: {
+                assignment: opdrachtId,
+                group: groepId
+            },
+            select: {
+                id: true,
+            }
+        });
+
+        const resultaten = conversaties.map((conversatie) => 
+            `/klassen/${klasId}/opdrachten/${opdrachtId}/groepen/${groepId}/conversaties/${conversatie.id}`
+        );
+
+        res.status(200).send({conversaties: resultaten});
+    } catch (e) {
+        res.status(500).send({error: "interne fout"});
+    }
 }
 
-export async function groep_maak_conversatie(req: Request, res: Response) {
+export async function groepMaakConversatie(req: Request, res: Response) {
     res.status(501);
 }
 
@@ -12,6 +54,6 @@ export async function conversatie(req: Request, res: Response) {
     res.status(501);
 }
 
-export async function verwijder_conversatie(req: Request, res: Response) {
+export async function verwijderConversatie(req: Request, res: Response) {
     res.status(501);
 }
--- src/backend/backend_opzet/routes/klassen/opdrachten/groepen/conversaties/conversaties_router.ts
+++ src/backend/backend_opzet/routes/klassen/opdrachten/groepen/conversaties/conversaties_router.ts
@@ -1,9 +1,9 @@
 import {Router} from "express";
 import {
     conversatie,
-    groep_conversaties,
-    groep_maak_conversatie,
-    verwijder_conversatie
+    groepConversaties,
+    groepMaakConversatie,
+    verwijderConversatie
 } from "../../../../../controllers/klassen/opdrachten/groepen/conversaties/conversaties_controller.ts";
 import berichten_router from "./berichten/berichten_router.ts";
 
@@ -12,7 +12,7 @@ export default router
 
 router.use("/:coversatie_id/berichten", berichten_router);
 
-router.get("/", groep_conversaties);
-router.post("/", groep_maak_conversatie);
+router.get("/", groepConversaties);
+router.post("/", groepMaakConversatie);
 router.get("/:conversatie_id", conversatie);
-router.delete("/:conversatie_id", verwijder_conversatie);
+router.delete("/:conversatie_id", verwijderConversatie);
