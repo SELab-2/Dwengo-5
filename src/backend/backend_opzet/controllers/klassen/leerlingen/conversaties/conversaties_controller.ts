8dbf77bfce22364c5967b129f7aa168f56635907
jarob <jaron.batens@ugent.be>
2025-03-02 11:40:40 +0100

    fix fixen

--- src/backend/backend_opzet/controllers/klassen/leerlingen/conversaties/conversaties_controller.ts
+++ src/backend/backend_opzet/controllers/klassen/leerlingen/conversaties/conversaties_controller.ts
@@ -3,19 +3,3 @@ import { Request, Response } from "express";
 export async function leerling_conversaties(req: Request, res: Response) {
   res.status(501);
 }
-
-export async function vraag(req: Request, res: Response) {
-  res.status(501);
-}
-
-export async function vraag_aanmaken(req: Request, res: Response) {
-  res.status(501);
-}
-
-export async function vraag_verwijderen(req: Request, res: Response) {
-  res.status(501);
-}
-
-export async function beantwoord_vraag(req: Request, res: Response) {
-  res.status(501);
-}
