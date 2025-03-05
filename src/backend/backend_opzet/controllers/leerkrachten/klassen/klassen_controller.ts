50279c372e57d67b1bdba4b2dcb6f05bfed1ebd9
jarob <jaron.batens@ugent.be>
2025-03-05 22:25:55 +0100

    exception aanpassen voor async

--- src/backend/backend_opzet/controllers/leerkrachten/klassen/klassen_controller.ts
+++ src/backend/backend_opzet/controllers/leerkrachten/klassen/klassen_controller.ts
@@ -1,16 +1,16 @@
-import {Request, Response} from "express";
+import {NextFunction, Request, Response} from "express";
 import {prisma, website_base} from "../../../index.ts";
 import {z} from "zod";
 import {ExpressException} from "../../../exceptions/ExpressException.ts";
 
-export async function leerkracht_klassen(req: Request, res: Response) {
+export async function leerkracht_klassen(req: Request, res: Response, next: NextFunction) {
     const teacherId = z.coerce.number().safeParse(req.params.leerkracht_id);
-    if (!teacherId.success) throw new ExpressException(400, "invalie teacherId");
+    if (!teacherId.success) throw new ExpressException(400, "invalie teacherId", next);
 
     const leerkracht = await prisma.teacher.findUnique({
         where: {id: teacherId.data}
     });
-    if (!leerkracht) throw new ExpressException(404, "teacher not found");
+    if (!leerkracht) throw new ExpressException(404, "teacher not found", next);
 
     const klassen = await prisma.classTeacher.findMany({
         where: {teachers_id: teacherId.data}
--- src/backend/backend_opzet/controllers/leerlingen/klassen/opdrachten/opdrachten_controller.ts
+++ src/backend/backend_opzet/controllers/leerlingen/klassen/opdrachten/opdrachten_controller.ts
@@ -1,19 +1,19 @@
-import {Request, Response} from "express";
+import {NextFunction, Request, Response} from "express";
 import {z} from "zod";
 import {prisma, website_base} from "../../../../index.ts";
 import {ExpressException} from "../../../../exceptions/ExpressException.ts";
 
-export async function leerling_opdrachten(req: Request, res: Response) {
+export async function leerling_opdrachten(req: Request, res: Response,next:NextFunction) {
     const studentId = z.coerce.number().safeParse(req.params.leerling_id);
-    if (!studentId.success) throw new ExpressException(400, "invalid studentId");
+    if (!studentId.success) throw new ExpressException(400, "invalid studentId",next);
 
     const student = await prisma.student.findUnique({
         where: {id: studentId.data}
     });
-    if (!student) throw new ExpressException(404, "student not found");
+    if (!student) throw new ExpressException(404, "student not found",next);
 
     const classId = z.coerce.number().safeParse(req.params.klas_id);
-    if (!classId.success) throw new ExpressException(404, "class not found");
+    if (!classId.success) throw new ExpressException(404, "class not found",next);
 
     const assignments = await prisma.assignment.findMany({
         where: {
