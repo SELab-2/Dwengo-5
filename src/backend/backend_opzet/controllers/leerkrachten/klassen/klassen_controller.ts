a478cd481961b800db3eff54b7009da3906d1416
jarob <jaron.batens@ugent.be>
2025-03-05 10:40:13 +0100

    supermooie refactoring

--- src/backend/backend_opzet/controllers/leerkrachten/klassen/klassen_controller.ts
+++ src/backend/backend_opzet/controllers/leerkrachten/klassen/klassen_controller.ts
@@ -1,31 +1,20 @@
 import {Request, Response} from "express";
 import {prisma, website_base} from "../../../index.ts";
 import {z} from "zod";
+import {ExpressException} from "../../../exceptions/ExpressException.ts";
 
 export async function leerkracht_klassen(req: Request, res: Response) {
-    try {
-        let teacherId = z.number().safeParse(req.params.leerkracht_id);
-        if (!teacherId.success) {
-            res.status(400).send({error: "invalie teacherId"});
-            return;
-        }
-        const leerkracht = await prisma.teacher.findUnique({
-            where: {
-                id: teacherId.data
-            }
-        });
-        if (!leerkracht) {
-            res.status(404).send({error: "teacher not found"});
-            return;
-        }
-        const klassen = await prisma.classTeacher.findMany({
-            where: {
-                teachers_id: teacherId.data
-            }
-        });
-        let klassen_links = klassen.map(klas => website_base + "/klassen/" + klas.classes_id);
-        res.status(200).send(klassen_links);
-    } catch (e) {
-        res.status(500).send({error: "internal error"})
-    }
+    const teacherId = z.number().safeParse(req.params.leerkracht_id);
+    if (!teacherId.success) throw new ExpressException(400, "invalie teacherId");
+
+    const leerkracht = await prisma.teacher.findUnique({
+        where: {id: teacherId.data}
+    });
+    if (!leerkracht) throw new ExpressException(404, "teacher not found");
+
+    const klassen = await prisma.classTeacher.findMany({
+        where: {teachers_id: teacherId.data}
+    });
+    const klassen_links = klassen.map(klas => website_base + "/klassen/" + klas.classes_id);
+    res.status(200).send(klassen_links);
 }
--- src/backend/backend_opzet/controllers/leerkrachten/leerkrachten_controller.ts
+++ src/backend/backend_opzet/controllers/leerkrachten/leerkrachten_controller.ts
@@ -1,52 +1,33 @@
 import {Request, Response} from "express";
 import {z} from "zod";
 import {prisma} from "../../index.ts";
-import {PrismaClientKnownRequestError} from "@prisma/client/runtime/library";
+import {ExpressException} from "../../exceptions/ExpressException.ts";
 
 export async function leerkracht(req: Request, res: Response) {
-    try {
-        let teacherId = z.number().safeParse(req.params.leerkracht_id);
-        if (!teacherId.success) {
-            res.status(400).send({error: "invalid teacherId"});
-            return;
+    const teacherId = z.number().safeParse(req.params.leerkracht_id);
+    if (!teacherId.success) throw new ExpressException(400, "invalid teacherId");
+
+    const teacher = await prisma.teacher.findUnique({
+        where: {
+            id: teacherId.data
         }
-        const teacher = await prisma.teacher.findUnique({
-            where: {
-                id: teacherId.data
-            }
-        });
-        if (!teacher) {
-            res.status(404).send({error: "teacher not found"});
-            return;
-        }
-        res.status(200).send({naam: teacher.username});
-    } catch (e) {
-        res.status(500).send({error: "internal error"})
-    }
+    });
+    if (!teacher) throw new ExpressException(404, "teacher not found");
+
+    res.status(200).send({naam: teacher.username});
 }
 
 export async function verwijder_leerkracht(req: Request, res: Response) {
-    try {
-        let teacherId = z.number().safeParse(req.params.leerkracht_id);
-        if (!teacherId.success) {
-            res.status(400).send({error: "invalid teacherId"});
-            return;
-        }
-        try {
-            await prisma.teacher.delete({
-                where: {
-                    id: teacherId.data
-                }
-            })
-        } catch (e) {
-            if (e instanceof PrismaClientKnownRequestError && e.code === "P2025") {
-                res.status(404).send({error: "teacher doesn't exist"});
-                return;
-            }
-            throw e;
-        }
-        res.status(200).send();
-    } catch (e) {
-        res.status(500).send({error: "internal error"})
-    }
+    const teacherId = z.number().safeParse(req.params.leerkracht_id);
+    if (!teacherId.success) throw new ExpressException(400, "invalid teacherId");
+
+    const teacher = prisma.teacher.findUnique({
+        where: {id: teacherId.data}
+    });
+    if (!teacher) throw new ExpressException(404, "teacher doesn't exist");
+
+    await prisma.teacher.delete({
+        where: {id: teacherId.data}
+    });
+    res.status(200).send();
 }
