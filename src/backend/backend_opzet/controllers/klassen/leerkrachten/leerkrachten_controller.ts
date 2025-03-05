9f34cde481a959da7d7542566dd9ed7d756b72ae
jarob <jaron.batens@ugent.be>
2025-03-05 15:11:51 +0100

    klas_verwijder_leerkracht

--- src/backend/backend_opzet/controllers/klassen/leerkrachten/leerkrachten_controller.ts
+++ src/backend/backend_opzet/controllers/klassen/leerkrachten/leerkrachten_controller.ts
@@ -2,6 +2,12 @@ import {Request, Response} from "express";
 import {z} from "zod";
 import {ExpressException} from "../../../exceptions/ExpressException.ts";
 import {prisma, website_base} from "../../../index.ts";
+import {
+    doesTokenBelongToStudentInClass,
+    doesTokenBelongToTeacher,
+    doesTokenBelongToTeacherInClass,
+    getJWToken
+} from "../../authenticatie/extra_auth_functies.ts";
 
 export async function klas_leerkrachten(req: Request, res: Response) {
     const classId = z.number().safeParse(req.params.klas_id);
@@ -11,6 +17,12 @@ export async function klas_leerkrachten(req: Request, res: Response) {
         where: {id: classId.data}, include: {classes_teachers: true}
     });
     if (!classroom) throw new ExpressException(404, "class not found");
+
+    const token = getJWToken(req);
+    const auth1 = await doesTokenBelongToTeacherInClass(classId.data, token);
+    const auth2 = await doesTokenBelongToStudentInClass(classId.data, token);
+    if (!(auth1.success || auth2.success))
+        throw new ExpressException(403, auth1.errorMessage + " and " + auth2.errorMessage);
 
     const teacherLinks = classroom.classes_teachers.map(teacher =>
         website_base + `/leerkrachten/${teacher.teachers_id}`);
@@ -22,5 +34,26 @@ export async function voeg_leerkracht_toe(req: Request, res: Response) {
 }
 
 export async function klas_verwijder_leerkracht(req: Request, res: Response) {
+    const classId = z.number().safeParse(req.params.klas_id);
+    const teacherId = z.number().safeParse(req.params.leerkracht_id);
+    if (!classId.success) throw new ExpressException(400, "invalid classId");
+    if (!teacherId.success) throw new ExpressException(400, "invalid teacherId");
 
+    const classroom = await prisma.class.findUnique({
+        where: {id: classId.data}
+    });
+    if (!classroom) throw new ExpressException(404, "class not found");
+
+    const token = getJWToken(req);
+    const auth1 = await doesTokenBelongToTeacher(teacherId.data, token);
+    if (!auth1.success) throw new ExpressException(403, auth1.errorMessage);
+
+    // todo: hoe doen we het dat als een klas zijn laatste leerkracht
+    // todo: verliest, deze ook verwijderd wordt. is dit db of api taak
+    await prisma.teacher.delete({
+        where: {
+            id: teacherId.data
+        }
+    });
+    res.status(200).send();
 }
