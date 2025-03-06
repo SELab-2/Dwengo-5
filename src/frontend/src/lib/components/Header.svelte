2c1c5204c2eb86be312fe933ede7a6098820a3db
mvdrputt <Mirval.Vanderputten@UGent.be>
2025-03-06 13:42:44 +0100

    added homepage, header, footer

---
+++ src/frontend/src/lib/components/Header.svelte
@@ -0,0 +1,32 @@
+<script>
+    import { createEventDispatcher } from "svelte";
+
+    let items = ["Home", "Dashboard", "Catalog", "Classrooms", "Courses", "Create new course", "Over ons team"];
+    export let activeItem = "Home";
+    const dispatch = createEventDispatcher();
+</script>
+
+<header>
+    <h1>
+        <img src="/img/dwengo-groen-zwart.svg" alt="Dwengo Logo">
+    </h1>
+    
+</header>
+
+<style>
+    header {
+        background-color: #f7f7f7;
+    }
+    h1 {
+        text-align: left;
+        list-style-type: none;
+        display: flex;
+    }
+    img {
+        display: block;
+        max-width:230px;
+        max-height:95px;
+        width: auto;
+        height: auto;
+    }
+</style>
--- src/frontend/src/routes/+page.svelte
+++ src/frontend/src/routes/+page.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+    import Footer from "$lib/components/Footer.svelte";
     import LoginPage from "./login/loginpage.svelte";
 </script>
 <LoginPage/>
+<Footer/>
