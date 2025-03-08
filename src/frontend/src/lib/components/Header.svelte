57ba3f8b68226705b3d96cfb63209ad30a6ecc26
rslaus <slausrobbert@gmail.com>
2025-03-08 21:02:26 +0100

    Header adjustments made -space for pf picture -space for notification -space for search icon -space for language selecter

--- src/frontend/src/lib/components/Header.svelte
+++ src/frontend/src/lib/components/Header.svelte
@@ -16,6 +16,14 @@
     <h1>
         <img src="/images/dwengo-groen-zwart.svg" alt="Dwengo Logo">
         <Tab activeItem items={items} on:tabChange={tabChange} />
+        <!---todo hier moet nog een language selecter zoals dodona --->
+
+        <p class=profile_pic>   :/</p>
+        <div>
+            <p>Pedro</p>
+            <p style="font-size: medium;">Teacher</p>
+        </div>
+        <!---todo hier moet nog een vergrootglas en notifactiebel --->
     </h1>
     
 </header>
@@ -35,5 +43,12 @@
         max-height:95px;
         width: auto;
         height: auto;
+    }
+    .profile_pic {
+        margin-left: auto;
+        background-color: aquamarine;
+        border-radius: 50%;
+        width: 80px;
+        height: 80px;
     }
 </style>
