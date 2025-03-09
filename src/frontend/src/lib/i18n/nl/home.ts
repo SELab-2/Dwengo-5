5504c71db8604892488909a61a8e8dc25bc3a50d
mvdrputt <Mirval.Vanderputten@UGent.be>
2025-03-09 18:31:54 +0100

    change internationalisatie

--- src/frontend/src/lib/i18n/nl/home.ts
+++ src/frontend/src/lib/i18n/nl/home.ts
@@ -1,7 +1,7 @@
-export default{
+export default {
     large_title: 'Ons {interactief} leerplatform',
     box1_title: 'Vind Cursussen',
-    box1_text: 'Zoek naar cursussen die je interesseren',
-    box2_title: 'Ga naar je klaslokaalen',
-    box2_text: 'Ga naar je lessen en begin met leren',
+    box1_text: 'Zoek cursussen die je kunnen interesseren',
+    box2_title: 'Ga naar je klaslokalen',
+    box2_text: 'Ga naar je cursussen en begin met leren',
 }
