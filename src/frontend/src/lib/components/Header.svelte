e3d10dd558d0a963351d0fb910b01a4e89d2805a
rslaus <slausrobbert@gmail.com>
2025-03-09 11:41:56 +0100

    - new search bar - nicer ui for header - new global.css file with colors and fonts

--- src/frontend/src/lib/components/Header.svelte
+++ src/frontend/src/lib/components/Header.svelte
@@ -4,26 +4,25 @@
 
     let items = ["Home", "Catalog", "Classrooms", "Assignments"];
     export let activeItem = "Home";
-    const dispatch = createEventDispatcher();
-
-    const tabChange = (e) => {
-        activeItem = e.detail;
-        dispatch("updateActiveItem", activeItem);
-    };
 </script>
 
 <header>
     <h1>
         <img src="/images/dwengo-groen-zwart.svg" alt="Dwengo Logo">
-        <Tab activeItem items={items} on:tabChange={tabChange} />
+        <Tab activeItem items={items}/>
         <!---todo hier moet nog een language selecter zoals dodona --->
 
         <img src="/images/blank_avatar.png" alt="Avatar" class="profile_pic">
-        <div>
-            <p>Pedro</p>
-            <p style="font-size: medium;">Teacher</p>
+        <div style="display: flex; flex-direction: column; align-items: flex-start; padding: 0 5px; margin-top: 20px;">
+            <p style="margin: 0;">Pedro</p>
+            <p style="margin: 0; font-size: medium;">Teacher</p>
         </div>
-        <!---todo hier moet nog een vergrootglas en notifactiebel --->
+        <div class="search-box" style="padding: 0 5px; margin-top: 20px;">
+            <button class="btn-search">
+            <img src="/images/magnifying_glass.png" alt="Search" class="search-icon" />
+            </button>
+            <input type="text" class="input-search" placeholder="Type to Search...">
+        </div>
     </h1>
     
 </header>
@@ -45,8 +44,69 @@
     .profile_pic {
         margin-left: auto;
         border-radius: 50%;
-        width: 80px;
-        height: 80px;
+        width: 120px;
+        height: 120px;
         background-image: "";
+    }
+    *{
+        box-sizing: border-box;
+    }
+    .search-box{
+        width: fit-content;
+        height: fit-content;
+        position: relative;
+    }
+    .input-search{
+        height: 50px;
+        width: 50px;
+        border-style: none;
+        padding: 10px;
+        font-size: 18px;
+        letter-spacing: 2px;
+        outline: none;
+        border-radius: 25px;
+        transition: all .5s ease-in-out;
+        background-color: var(--dwengo-green);
+        padding-right: 40px;
+        color:#000000;
+    }
+    .input-search::placeholder{
+        color:black;
+        font-size: 18px;
+        letter-spacing: 2px;
+        font-weight: 100;
+    }
+    .btn-search{
+        width: 50px;
+        height: 50px;
+        border-style: none;
+        font-size: 20px;
+        font-weight: bold;
+        outline: none;
+        cursor: pointer;
+        border-radius: 50%;
+        position: absolute;
+        right: 0px;
+        color:black ;
+        background-color:transparent;
+        pointer-events: painted;  
+    }
+    .btn-search:focus ~ .input-search{
+        width: 300px;
+        border-radius: 0px;
+        background-color: transparent;
+        border-bottom:1px solid black;
+        transition: all 500ms cubic-bezier(0, 0.110, 0.35, 2);
+    }
+    .input-search:focus{
+        width: 300px;
+        border-radius: 0px;
+        background-color: transparent;
+        border-bottom:1px solid black;
+        transition: all 500ms cubic-bezier(0, 0.110, 0.35, 2);
+    }
+    .search-icon{
+        width: 30px;
+        height: 30px;
     }
 </style>
