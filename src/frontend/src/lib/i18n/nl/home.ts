8b6740fdbc4fe2579b52d08492ef097220fcd996
mvdrputt <Mirval.Vanderputten@UGent.be>
2025-03-09 19:35:48 +0100

    deleted old i18n folder

--- src/frontend/src/lib/i18n/nl/home.ts
+++
@@ -1,7 +0,0 @@
-export default {
-    large_title: 'Ons {interactief} leerplatform',
-    box1_title: 'Vind Cursussen',
-    box1_text: 'Zoek cursussen die je kunnen interesseren',
-    box2_title: 'Ga naar je klaslokalen',
-    box2_text: 'Ga naar je cursussen en begin met leren',
-}
