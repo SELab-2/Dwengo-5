8ade5c274db4f7e88cff0c8f68c64e0ca03ff632
mvdrputt <Mirval.Vanderputten@UGent.be>
2025-03-09 20:05:45 +0100

    import change

--- src/frontend/src/lib/components/LoginForm.svelte
+++ src/frontend/src/lib/components/LoginForm.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
     import { goto } from "$app/navigation";
     import { apiBaseUrl } from "../../config";
-    import { currentTranslations } from "../locales/i18n";
+    import { currentTranslations } from "$lib/locales/i18n";
     let email = "";
     let password = "";
     let errorMessage = "";
