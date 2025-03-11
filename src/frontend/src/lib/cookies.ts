b2f5233c8fa10f65a9ce801cc4ff496865557d0c
rslaus <slausrobbert@gmail.com>
2025-03-11 15:43:03 +0100

    Fixed comment from Mirval about date

--- src/frontend/src/lib/cookies.ts
+++ src/frontend/src/lib/cookies.ts
@@ -5,8 +5,7 @@ export function setCookies(name: string, value: string, days: number) {
 
     let expires = "";
     if (days) {
-        const date = new Date();
-        date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+        const date = new Date(Date.now() + days * 24 * 60 * 60 * 1000);
         expires = "; expires=" + date.toUTCString();
     }
     document.cookie = `${name}=${value}; path=/;${expires}`;
