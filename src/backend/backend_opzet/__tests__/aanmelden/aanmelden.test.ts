228857ec9b8e9780072c43282892d60fd0b938f4
mvdrputt <Mirval.Vanderputten@UGent.be>
2025-03-11 22:25:28 +0100

    translation tests aanmelden/aanmelden + extra test

--- src/backend/backend_opzet/__tests__/aanmelden/aanmelden.test.ts
+++ src/backend/backend_opzet/__tests__/aanmelden/aanmelden.test.ts
@@ -1,11 +1,19 @@
 import request, { Response } from "supertest";
-import { describe, expect, it } from "vitest";
-import index from "../../index.ts";
+import { describe, expect, it, afterEach } from "vitest";
+import index from '../../index.ts';
+
+const testLeerling = { naam: "Test student", wachtwoord: "SafePassword123" };
+const testLeerkracht = { naam: "Test teacher", wachtwoord: "StrongPassword123" };
+
+let leerlingToken = "";
+let leerkrachtToken = "";
+let leerlingId = "";
+let leerkrachtId = "";
 
 describe("aanmelden", () => {
-  it("inloggen faalt op niet bestaande leerling", async () => {
+  it("logging in fails on non-existent student", async () => {
     const leerling: any = {
-      naam: "Quintinius Hoedtius (bestaat niet)",
+      naam: "Quintinius Hoedtius (doesn't exist)",
       wachtwoord: "wachtw00rd",
     };
     let res: Response = await request(index)
@@ -14,7 +22,7 @@ describe("aanmelden", () => {
     expect(res.status).toBe(400);
   });
 
-  it("inloggen faalt op niet bestaande leerkracht", async () => {
+  it("logging in fails on non-existent teacher", async () => {
     const leerkracht: any = {
       naam: "Roberto Saulo",
       wachtwoord: "knuffelmuis123",
@@ -38,7 +46,7 @@ describe("aanmelden", () => {
     let res: Response = await request(index)
       .post("/aanmelden/leerlingen")
       .send(leerling_aanmelding_fout_wachtwoord);
-    expect(res.status).toBe(400);
+    expect(res.status).toBe(404);
     res = await request(index)
       .post("/aanmelden/leerlingen")
       .send(nieuwe_leerling);
@@ -52,7 +60,7 @@ describe("aanmelden", () => {
       .set(`Authorization`, `Bearer ${token}`);
   });
 
-  it("fout wachtwoord faalt", async () => {
+  it("wrong password fails", async () => {
     const nieuwe_leerkracht: any = {
       naam: "Roberto Saulo",
       wachtwoord: "knuffelmuis123",
@@ -60,12 +68,12 @@ describe("aanmelden", () => {
     await request(index).post("/leerkrachten").send(nieuwe_leerkracht);
     const leerkracht_aanmelding_fout_wachtwoord: any = {
       naam: "Roberto Saulo",
-      wachtwoord: "fout wachtwoord",
+      wachtwoord: "wrong password",
     };
     let res: Response = await request(index)
       .post("/aanmelden/leerkrachten")
       .send(leerkracht_aanmelding_fout_wachtwoord);
-    expect(res.status).toBe(400);
+    expect(res.status).toBe(404);
     res = await request(index)
       .post("/aanmelden/leerkrachten")
       .send(nieuwe_leerkracht);
@@ -79,3 +87,58 @@ describe("aanmelden", () => {
       .set(`Authorization`, `Bearer ${token}`);
   });
 });
+
+describe("log in - extra tests", () => {
+  afterEach(async () => {
+    if (leerlingId) {
+      await request(index)
+        .del(leerlingId)
+        .set("Authorization", `Bearer ${leerlingToken}`);
+    }
+    if (leerkrachtId) {
+      await request(index)
+        .del(leerkrachtId)
+        .set("Authorization", `Bearer ${leerkrachtToken}`);
+    }
+  });
+
+  it("successfully log in as student", async () => {
+    await request(index).post("/leerlingen").send(testLeerling);
+    let res: Response = await request(index)
+      .post("/aanmelden/leerlingen")
+      .send(testLeerling);
+    expect(res.status).toBe(200);
+    expect(res.body).toHaveProperty("token");
+    leerlingToken = res.body.token;
+    leerlingId = res.body.id;
+  });
+
+  it("successfully log in as teacher", async () => {
+    await request(index).post("/leerkrachten").send(testLeerkracht);
+    let res: Response = await request(index)
+      .post("/aanmelden/leerkrachten")
+      .send(testLeerkracht);
+    expect(res.status).toBe(200);
+    expect(res.body).toHaveProperty("token");
+    leerkrachtToken = res.body.token;
+    leerkrachtId = res.body.id;
+  });
+
+  it("logging in fails with empty password", async () => {
+    const leerling = { naam: "Test student", wachtwoord: "" };
+    let res: Response = await request(index)
+      .post("/aanmelden/leerlingen")
+      .send(leerling);
+    expect(res.status).toBe(404);
+  });
+
+  it("logging in fails with empty username", async () => {
+    const leerling = { naam: "", wachtwoord: "SafePassword123" };
+    let res: Response = await request(index)
+      .post("/aanmelden/leerlingen")
+      .send(leerling);
+    expect(res.status).toBe(404);
+  });
+
+
+})
