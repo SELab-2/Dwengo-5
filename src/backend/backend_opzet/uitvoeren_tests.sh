9ade5b6a9e0ac191f43ba117ccc2849ed762dc7c
Thorsten De Neve <thorsten.deneve@ugent.be>
2025-03-13 16:27:02 +0100

    Eerste aanpassing om minder geheugen te gebruiken met docker

--- src/backend/backend_opzet/uitvoeren_tests.sh
+++ src/backend/backend_opzet/uitvoeren_tests.sh
@@ -55,5 +55,6 @@ fi
 # Run the selected test
 PROJECT_ROOT=$(git rev-parse --show-toplevel)
 export TEST_FILE="$selected_test"
-TEST_FILE="$selected_test" docker compose -f $PROJECT_ROOT/test-docker-compose.yml up --build --abort-on-container-exit --exit-code-from backend
-docker compose -f $PROJECT_ROOT/test-docker-compose.yml down
+TEST_FILE="$selected_test" docker compose -f $PROJECT_ROOT/test-docker-compose.yml build --no-cache
+TEST_FILE="$selected_test" docker compose -f $PROJECT_ROOT/test-docker-compose.yml up --no-build  --abort-on-container-exit --exit-code-from backend
+docker compose -f $PROJECT_ROOT/test-docker-compose.yml down - v
