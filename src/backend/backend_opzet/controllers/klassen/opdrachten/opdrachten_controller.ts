81a54ca9762e7cf546cd5d443682dc906e22917e
Nyah Van Wayenberge <nyah.vanwayenberge@ugent.be>
2025-03-13 16:24:00 +0100

    klassen/opdrachten controller adjusted to use throwExpressException instead of ExpressException

--- src/backend/backend_opzet/controllers/klassen/opdrachten/opdrachten_controller.ts
+++ src/backend/backend_opzet/controllers/klassen/opdrachten/opdrachten_controller.ts
@@ -1,12 +1,12 @@
 import {NextFunction, Request, Response} from "express";
 import {prisma} from "../../../index.ts";
-import {ExpressException} from "../../../exceptions/ExpressException.ts";
+import {throwExpressException} from "../../../exceptions/ExpressException.ts";
 import {z} from "zod";
 
 // GET: /klassen/:klas_id/opdrachten
 export async function klasOpdrachten(req: Request, res: Response, next: NextFunction) {
   const classId = z.coerce.number().safeParse(req.params.klas_id);
-  if (!classId.success) throw new ExpressException(400, "invalid classId", next);
+  if (!classId.success) return throwExpressException(400, "invalid classId", next);
 
   const klas = prisma.class.findUnique({
     where: {
@@ -14,7 +14,7 @@ export async function klasOpdrachten(req: Request, res: Response, next: NextFunc
     },
   });
 
-  if (klas === null) throw new ExpressException(404, "class not found", next);
+  if (klas === null) return throwExpressException(404, "class not found", next);
 
   const assignments = await prisma.assignment.findMany({
     where: {
@@ -32,23 +32,23 @@ export async function klasOpdrachten(req: Request, res: Response, next: NextFunc
 // POST /klassen/:klas_id/opdrachten
 export async function maakOpdracht(req: Request, res: Response, next: NextFunction) {
   const classId = z.coerce.number().safeParse(req.params.klas_id);
-  if (!classId.success) return new ExpressException(400, "invalid classId", next);
+  if (!classId.success) return throwExpressException(400, "invalid classId", next);
 
   let leerpad_id =  z.string().safeParse(req.body.learning_path.split('/').pop());
-  if (!leerpad_id.success) return new ExpressException(400, "invalid learning_path", next);
+  if (!leerpad_id.success) return throwExpressException(400, "invalid learning_path", next);
 
   let deadline = z.coerce.date().safeParse(req.body.deadline);
-  if (!deadline.success) return new ExpressException(400, "invalid deadline", next);
+  if (!deadline.success) return throwExpressException(400, "invalid deadline", next);
 
   let name = z.coerce.string().safeParse(req.body.name);
-    if (!name.success) return new ExpressException(400, "invalid name", next);
+    if (!name.success) return throwExpressException(400, "invalid name", next);
 
   const klas = prisma.class.findUnique({
     where: {
       id: classId.data,
     },
   });
-  if (klas === null) throw new ExpressException(404, "class not found", next);
+  if (klas === null) return throwExpressException(404, "class not found", next);
 
   console.log(leerpad_id);
 
@@ -59,7 +59,7 @@ export async function maakOpdracht(req: Request, res: Response, next: NextFuncti
   });
 
   if (leerpad === null) {
-    throw new ExpressException(400, `learningPath with uuid: ${leerpad_id} does not exist`, next);
+    return throwExpressException(400, `learningPath with uuid: ${leerpad_id} does not exist`, next);
   }
 
 
@@ -80,15 +80,15 @@ export async function klasOpdracht(req: Request, res: Response, next: NextFuncti
   const classId = z.coerce.number().safeParse(req.params.klas_id);
   const assignmentId = z.coerce.number().safeParse(req.params.opdracht_id);
 
-  if (!classId.success) throw new ExpressException(400, "invalid classId", next);
-  if (!assignmentId.success) throw new ExpressException(400, "invalid assignmentId", next);
+  if (!classId.success) return throwExpressException(400, "invalid classId", next);
+  if (!assignmentId.success) return throwExpressException(400, "invalid assignmentId", next);
 
   const classroom = await prisma.class.findUnique({
     where: {
       id: classId.data,
     },
   });
-  if (classroom === null) throw new ExpressException(404, "class not found", next);
+  if (classroom === null) return throwExpressException(404, "class not found", next);
 
   const assignment = await prisma.assignment.findUnique({
     where: {
@@ -97,7 +97,7 @@ export async function klasOpdracht(req: Request, res: Response, next: NextFuncti
     },
   });
 
-  if (assignment === null) throw new ExpressException(404, "assignment not found", next);
+  if (assignment === null) return throwExpressException(404, "assignment not found", next);
 
   const opdrachtLink =
     `/klassen/${classId}/opdrachten/${assignmentId}`;
@@ -114,15 +114,15 @@ export async function verwijderOpdracht(req: Request, res: Response, next: NextF
   const classId = z.coerce.number().safeParse(req.params.klas_id);
   const assignmentId = z.coerce.number().safeParse(req.params.opdracht_id);
 
-  if (!classId.success) throw new ExpressException(400, "invalid classId", next);
-  if (!assignmentId.success) throw new ExpressException(400, "invalid assignmentId", next);
+  if (!classId.success) return throwExpressException(400, "invalid classId", next);
+  if (!assignmentId.success) return throwExpressException(400, "invalid assignmentId", next);
 
   const klas = prisma.class.findUnique({
     where: {
       id: classId.data,
     },
   });
-  if (klas === null) throw new ExpressException(404, "class not found", next);
+  if (klas === null) return throwExpressException(404, "class not found", next);
 
   const assignment = await prisma.assignment.findUnique({
     where: {
@@ -130,7 +130,7 @@ export async function verwijderOpdracht(req: Request, res: Response, next: NextF
     },
   });
 
-  if (assignment === null) throw new ExpressException(404, "assignment not found", next);
+  if (assignment === null) return throwExpressException(404, "assignment not found", next);
 
   await prisma.assignment.delete({
     where: { id: assignmentId.data },
