c11f8bb4f03b40a2c92a1e6365a8d04f637117fc
jarob <jaron.batens@ugent.be>
2025-03-17 14:57:27 +0100

    learningpaths.test.ts fix

--- src/backend/__tests__/learningpaths/learningpaths.test.ts
+++ src/backend/__tests__/learningpaths/learningpaths.test.ts
@@ -1,11 +1,12 @@
 import request from "supertest";
 import {describe, expect, it} from "vitest";
 import index from '../../index.ts';
+import {z} from "zod";
+import {learningobjectRexp} from "../../help/validation.ts";
 
-// GET /learningpaths?language
 describe("learningpaths", (): void => {
     it("get all learning_paths configures in seed.ts", async (): Promise<void> => {
-        let res = await request(index).get("/learningpaths?language=en");
+        const res = await request(index).get("/learningpaths?language=en");
         expect(res.status).toBe(200);
         expect(res.body.learningpaths).toHaveLength(2);
         expect(res.body.learningpaths[0]).toBe("/learningpaths/550e8400-e29b-41d4-a716-446655440000");
@@ -13,23 +14,28 @@ describe("learningpaths", (): void => {
     });
 });
 
-// GET /learningpaths/:learningpathId
 describe("learningpath", (): void => {
-    it("get all learning_paths configures in seed.ts", async (): Promise<void> => {
-        let res = await request(index).get("/learningpaths/550e8400-e29b-41d4-a716-446655440001");
+    it("get \"physics concepts\" learningpath ", async (): Promise<void> => {
+        const res = await request(index).get("/learningpaths/550e8400-e29b-41d4-a716-446655440001");
         expect(res.status).toBe(200);
         expect(res.body.name).toBe("550e8400-e29b-41d4-a716-446655440001");
         expect(res.body.content).toBe("/learningpaths/550e8400-e29b-41d4-a716-446655440001/content");
         expect(res.body.image).toBe(null);
-        expect(res.body.description).toBe("")
+        expect(res.body.description).toBe("Basic physics concepts");
     });
 });
 
-// GET /learningpaths/:learningpathId/content
 describe("learningpath", (): void => {
-    // todo ik zit vast
-    it("get all learning_paths configures in seed.ts", async (): Promise<void> => {
-        let res = await request(index).get("/learningpaths/550e8400-e29b-41d4-a716-446655440001/content");
+    it("get content of learningpath", async (): Promise<void> => {
+        const res = await request(index).get("/learningpaths/550e8400-e29b-41d4-a716-446655440001/content");
         expect(res.status).toBe(200);
+        expect(z.array(z.object({
+            eerste_object_in_graaf: z.coerce.boolean(),
+            learningobject: z.string().regex(learningobjectRexp),
+            volgende: z.array(z.object({
+                learningobject: z.string().regex(learningobjectRexp),
+                vereisten: z.tuple([z.number(), z.number()])
+            }))
+        })).safeParse(res.body).success).toBe(true);
     });
 });
