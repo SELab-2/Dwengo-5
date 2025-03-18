e02b1b1e231b532768edc92fe5c8bd7a585ef69e
Thorsten De Neve <thorsten.deneve@ugent.be>
2025-03-18 17:41:38 +0100

    Toevoegen aan andere bestanden

--- src/backend/backend_opzet/uitvoeren_tests_all.sh
+++ src/backend/backend_opzet/uitvoeren_tests_all.sh
@@ -7,3 +7,4 @@ PROJECT_ROOT=$(git rev-parse --show-toplevel)
 export TEST_FILE="$selected_test"
 TEST_FILE="$selected_test" docker compose -f $PROJECT_ROOT/test-docker-compose.yml up --build --abort-on-container-exit --exit-code-from backend
 docker compose -f $PROJECT_ROOT/test-docker-compose.yml down
+docker system prune -a -f
