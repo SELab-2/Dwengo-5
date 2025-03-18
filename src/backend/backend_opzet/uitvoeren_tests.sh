4533c71c2ce33d5bca112796da7756fbc790879c
Thorsten De Neve <thorsten.deneve@ugent.be>
2025-03-18 17:39:49 +0100

    Na uitvoeren_tests.sh wordt docker ruimte reclaimed

--- src/backend/backend_opzet/uitvoeren_tests.sh
+++ src/backend/backend_opzet/uitvoeren_tests.sh
@@ -57,4 +57,5 @@ PROJECT_ROOT=$(git rev-parse --show-toplevel)
 export TEST_FILE="$selected_test"
 TEST_FILE="$selected_test" docker compose -f $PROJECT_ROOT/test-docker-compose.yml build --no-cache
 TEST_FILE="$selected_test" docker compose -f $PROJECT_ROOT/test-docker-compose.yml up --no-build  --abort-on-container-exit --exit-code-from backend
-docker compose -f $PROJECT_ROOT/test-docker-compose.yml down - v
+docker compose -f $PROJECT_ROOT/test-docker-compose.yml down -v
+docker system prune -a -f
