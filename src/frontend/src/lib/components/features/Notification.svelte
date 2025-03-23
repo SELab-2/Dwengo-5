95e0dbf0beea9da4aeb298e57f216d67d15217f8
rslaus <slausrobbert@gmail.com>
2025-03-23 18:35:45 +0100

    showing the amount of notifications (basic)

--- src/frontend/src/lib/components/features/Notification.svelte
+++ src/frontend/src/lib/components/features/Notification.svelte
@@ -1,13 +1,25 @@
 <script lang="ts">
-
 </script>
 
 <div class="notification-center">
-    <button class="bell"></button>
+    <div class="notification-icon">
+        <button class="bell"></button> <!--TODO show the notifications-->
+        <p class="amount">3</p> <!-- TODO Amount of notifications -->
+    </div>
 </div>
 
 <style>
-    .bell{
+    .notification-center {
+        display: flex;
+        align-items: center;
+    }
+
+    .notification-icon {
+        position: relative; /* Enables absolute positioning for children */
+        display: inline-block;
+    }
+
+    .bell {
         background-image: url("../../../../static/images/notification_bell.png");
         background-color: transparent;
         background-size: cover;
@@ -15,5 +27,20 @@
         height: 30px;
         border: none;
         cursor: pointer;
+    }
+
+    .amount {
+        position: absolute;
+        top: -5px;  /* Adjust to move upwards */
+        right: -5px; /* Adjust to move right */
+        background-color: #FF0000;
+        color: white;
+        border-radius: 50%;
+        width: 18px;
+        height: 18px;
+        text-align: center;
+        font-size: 12px;
+        line-height: 18px;
+        font-weight: bold;
     }
 </style>
