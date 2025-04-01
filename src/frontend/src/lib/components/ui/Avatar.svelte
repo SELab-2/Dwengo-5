65c6f3137145a644da32e73e77380dee2979e398
Nyah Van Wayenberge <nyah.vanwayenberge@ugent.be>
2025-04-01 15:51:12 +0200

    avatar color base on the name so is is always the same color

--- src/frontend/src/lib/components/ui/Avatar.svelte
+++ src/frontend/src/lib/components/ui/Avatar.svelte
@@ -1,17 +1,24 @@
 <script lang="ts">
     export let name: string = "";
+    export let color: string = "";
 
     //Change if we get full name to first leter of name and first letter of second name.
     const initials: string = name.charAt(0).toUpperCase();
 
-    function getRandomColor() {
-        const hue = Math.floor(Math.random() * 360);
-        const saturation = Math.floor(Math.random() * 30) + 50;
-        const lightness = Math.floor(Math.random() * 20) + 70;
+    // TODO: current way to make sure the same name has the same color -> find a better way
+    function hashStringToNumber(str: string): number {
+        return Array.from(str).reduce((acc, char) => acc + char.charCodeAt(0), 0);
+    }
+
+    function getRandomColor(name: string) {
+        const hash = hashStringToNumber(name);
+        const hue = hash % 360;
+        const saturation = 50 + (hash % 30);
+        const lightness = 70 + (hash % 20);
         return `hsl(${hue}, ${saturation}%, ${lightness}%)`;
     }
 
-    let backgroundColor = getRandomColor();
+    let backgroundColor = getRandomColor(name);
 </script>
 
 <div class="avatar" style="background-color: {backgroundColor};">
