e7cf4876799a322dc556cf5fbb6ef885feb59236
Kamiel Moeyersoon <moeyersoon.kamiel@gmail.com>
2025-05-17 12:29:58 +0200

    make sure we can at least read an unexpected error in the console

--- src/backend/exceptions/exceptionMiddleware.ts
+++ src/backend/exceptions/exceptionMiddleware.ts
@@ -1,7 +1,16 @@
-import {NextFunction, Request, Response} from "express"
-import {ExpressException} from "./ExpressException.ts";
+import { NextFunction, Request, Response } from "express";
+import { ExpressException } from "./ExpressException.ts";
 
-export function exceptionHandler(err: Error, _req: Request, res: Response, _next: NextFunction): void {
-    if (err instanceof ExpressException) res.status(err.status).send({error: err.message});
-    else res.status(500).send({error: "internal error"});
+export function exceptionHandler(
+    err: Error,
+    _req: Request,
+    res: Response,
+    _next: NextFunction
+): void {
+    if (err instanceof ExpressException) {
+        res.status(err.status).send({ error: err.message });
+    } else {
+        console.error(err);
+        res.status(500).send({ error: "internal error" });
+    }
 }
