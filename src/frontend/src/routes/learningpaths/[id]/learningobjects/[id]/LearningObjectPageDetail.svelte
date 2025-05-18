9ed4ce678821d04398540f58f21845c8c43ba6b5
quindh <quintendhoedt@gmail.com>
2025-05-18 12:19:52 +0200

    changes

--- src/frontend/src/routes/learningpaths/[id]/learningobjects/[id]/LearningObjectPageDetail.svelte
+++ src/frontend/src/routes/learningpaths/[id]/learningobjects/[id]/LearningObjectPageDetail.svelte
@@ -1,11 +1,11 @@
 <script lang=ts>
     import { onMount } from "svelte";
-    import { currentTranslations } from "../../../lib/locales/i18n.ts";
-    import Header from "../../../lib/components/layout/Header.svelte";
-    import Footer from "../../../lib/components/layout/Footer.svelte";
-    import "../../../lib/styles/global.css";
-    import { apiRequest } from "../../../lib/api.ts";
-    import { routeTo } from '../../../lib/route.ts';
+    import { currentTranslations } from "../../../../../lib/locales/i18n.ts";
+    import Header from "../../../../../lib/components/layout/Header.svelte";
+    import Footer from "../../../../../lib/components/layout/Footer.svelte";
+    import "../../../../../lib/styles/global.css";
+    import { apiRequest } from "../../../../../lib/api.ts";
+    import { routeTo } from '../../../../../lib/route.ts';
 
     let id : string | undefined;
     let loading = true;
