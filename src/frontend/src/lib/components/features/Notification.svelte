5a4b949907107269de27b5935fd565af55e8f99c
Kamiel Moeyersoon <moeyersoon.kamiel@gmail.com>
2025-05-19 20:44:35 +0200

    make notification amount badge clickable and improve look

--- src/frontend/src/lib/components/features/Notification.svelte
+++ src/frontend/src/lib/components/features/Notification.svelte
@@ -9,9 +9,11 @@
     function handleClickOutside(event: MouseEvent) {
         const element = document.querySelector('.notification-list');
         const bell = document.querySelector('.bell');
+        const amount = document.querySelector('.amount');
         if (
             element && !element.contains(event.target as Node) &&
-            bell && !bell.contains(event.target as Node)
+            bell && !bell.contains(event.target as Node) && 
+            amount && !amount.contains(event.target as Node)
         ) {
             showNotifications = false;
             tick(); 
@@ -129,7 +131,13 @@
                 >
                 </button>
 
-                <p class="amount">{ questions.length + invites.length }</p>
+                <button
+                    type="button"
+                    class="amount"
+                    on:click={() => showNotifications = !showNotifications}
+                >
+                    { questions.length + invites.length }
+                </button>
             </div>
 
             {#if showNotifications}
@@ -179,6 +187,7 @@
         cursor: pointer;
     }
     .amount {
+        all: unset; 
         position: absolute;
         top: -5px;
         right: -5px;
@@ -191,6 +200,8 @@
         font-size: 12px;
         line-height: 18px;
         font-weight: bold;
+        cursor: pointer;
+        display: block;
     }
 
     .notif-item {
