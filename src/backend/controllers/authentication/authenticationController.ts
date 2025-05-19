98c3c7b9cc20569a5ae8fb6856117a42c0c4b779
jaronbatens <jaron.batens@ugent.Be>
2025-05-19 22:42:57 +0200

    cheap fix

--- src/backend/controllers/authentication/authenticationController.ts
+++ src/backend/controllers/authentication/authenticationController.ts
@@ -5,32 +5,35 @@ import bcrypt from "bcryptjs";
 import {JWT_SECRET, prisma} from "../../index.ts";
 import jwt from "jsonwebtoken";
 import {userLink} from "../../help/links.ts";
-import {studentToLink, teacherToLink} from "../../__tests__/helperFunctions.ts";
 
 export const login = async (req: Request, res: Response, next: NextFunction) => {
-    const email = z.string().email().safeParse(req.body.email);
-    const password = z.string().safeParse(req.body.password);
+    try {
+        const email = z.string().email().safeParse(req.body.email);
+        const password = z.string().safeParse(req.body.password);
 
-    if (!email.success) return throwExpressException(400, "invalid email", next);
-    if (!password.success) return throwExpressException(400, "invalid password", next);
+        if (!email.success) return throwExpressException(400, "invalid email", next);
+        if (!password.success) return throwExpressException(400, "invalid password", next);
 
-    const user = await prisma.user.findUnique({where: {email: email.data}});
-    if (!user) return throwExpressException(404, "user not found", next);
+        const user = await prisma.user.findUnique({where: {email: email.data}});
+        if (!user) return throwExpressException(404, "user not found", next);
 
-    const isPasswordValid = await bcrypt.compare(password.data, user.password);
-    if (!isPasswordValid) return throwExpressException(401, "wrong password", next);
+        const isPasswordValid = await bcrypt.compare(password.data, user.password);
+        if (!isPasswordValid) return throwExpressException(401, "wrong password", next);
 
-    const isStudent = !!(await prisma.student.findUnique({where: {id: user.id}}));
+        const isStudent = !!(await prisma.student.findUnique({where: {id: user.id}}));
 
-    const token = jwt.sign(
-        {id: user.id, email: user.email, usertype: isStudent ? "student" : "teacher"},
-        JWT_SECRET,
-        {expiresIn: "8h"}
-    );
-    res.status(200).send({
-        token: token,
-        user: userLink(user.id)
-    });
+        const token = jwt.sign(
+            {id: user.id, email: user.email, usertype: isStudent ? "student" : "teacher"},
+            JWT_SECRET,
+            {expiresIn: "8h"}
+        );
+        res.status(200).send({
+            token: token,
+            user: userLink(user.id)
+        });
+    } catch (e) {
+        return throwExpressException(500, "" + e, next);
+    }
 };
 
 export async function register(req: Request, res: Response, next: NextFunction) {
--- src/backend/controllers/authentication/authenticationMiddleware.ts
+++ src/backend/controllers/authentication/authenticationMiddleware.ts
@@ -5,26 +5,30 @@ import {throwExpressException} from "../../exceptions/ExpressException.ts";
 import {z} from "zod";
 
 export function authenticate(req: Request, _res: Response, next: NextFunction) {
-    const userId = z.coerce.number().safeParse(req.params.userId);
-    if (!userId.success) return throwExpressException(400, "invalid userId", next);
-
-    const authHeader = req.headers.authorization;
-    if (!authHeader || !authHeader.startsWith("Bearer "))
-        return throwExpressException(401, "no token sent", next);
-    const token = authHeader.slice(7); // cut "Bearer "
-    let payload: JwtPayload;
     try {
-        payload = jwt.verify(token, JWT_SECRET) as JwtPayload;
-    } catch (err: any) {
-        if (err.name === "TokenExpiredError")
-            return throwExpressException(401, "expired token", next);
-        throw err;
+        const userId = z.coerce.number().safeParse(req.params.userId);
+        if (!userId.success) return throwExpressException(400, "invalid userId", next);
+
+        const authHeader = req.headers.authorization;
+        if (!authHeader || !authHeader.startsWith("Bearer "))
+            return throwExpressException(401, "no token sent", next);
+        const token = authHeader.slice(7); // cut "Bearer "
+        let payload: JwtPayload;
+        try {
+            payload = jwt.verify(token, JWT_SECRET) as JwtPayload;
+        } catch (err: any) {
+            if (err.name === "TokenExpiredError")
+                return throwExpressException(401, "expired token", next);
+            throw err;
+        }
+
+        if (!payload || typeof payload !== "object" || !payload.id)
+            return throwExpressException(401, "invalid token", next);
+        if (Number(payload.id) !== userId.data)
+            return throwExpressException(401, "wrong token", next);
+
+        next();
+    } catch (e) {
+        return throwExpressException(500, "" + e, next);
     }
-
-    if (!payload || typeof payload !== "object" || !payload.id)
-        return throwExpressException(401, "invalid token", next);
-    if (Number(payload.id) !== userId.data)
-        return throwExpressException(401, "wrong token", next);
-
-    next();
 }
